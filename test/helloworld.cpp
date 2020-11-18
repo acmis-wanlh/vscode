eef036d072f1fd9c9ba50ce31867bc85f603655c
leon <lihang_wan@outlook.com>
2020-11-18 20:00:31 +0800

    remake

---
+++ test/helloworld.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 0;
+    for (int i = 0; i < 10; i++){
+        printf("%d", a);
+        a++;
+    }
+    printf("sdadsa");
+    return 0;
+}
